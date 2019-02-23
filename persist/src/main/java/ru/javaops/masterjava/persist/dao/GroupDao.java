1d0bd869f3f9492f5ef6bf304eb458ecf63c8750
booktest <fonto.trg@gmail.com>
2019-02-23 10:29:12 +0300

    Lesson06 - HW5 dao test

---
+++ persist/src/main/java/ru/javaops/masterjava/persist/dao/GroupDao.java
@@ -0,0 +1,39 @@
+package ru.javaops.masterjava.persist.dao;
+
+import com.bertoncelj.jdbi.entitymapper.EntityMapperFactory;
+import one.util.streamex.StreamEx;
+import org.skife.jdbi.v2.sqlobject.BindBean;
+import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
+import ru.javaops.masterjava.persist.model.Group;
+
+import java.util.List;
+import java.util.Map;
+
+@RegisterMapperFactory(EntityMapperFactory.class)
+public abstract class GroupDao implements AbstractDao<Group> {
+
+    @SqlUpdate("TRUNCATE groups CASCADE ")
+    @Override
+    public abstract void clean();
+
+    @SqlQuery("SELECT * FROM groups ORDER BY name")
+    public abstract List<Group> getAll();
+
+    public Map<String, Group> getAsMap() {
+        return StreamEx.of(getAll()).toMap(Group::getName, g -> g);
+    }
+
+    @SqlUpdate("INSERT INTO groups (name, type, project_id)  VALUES (:name, CAST(:type AS group_type), :projectId)")
+    @GetGeneratedKeys
+    public abstract int insertGeneratedId(@BindBean Group groups);
+
+    @Override
+    public <T extends Group> T insert(T group) {
+        int id = insertGeneratedId(group);
+        group.setId(id);
+        return group;
+    }
+}
---
+++ persist/src/main/java/ru/javaops/masterjava/persist/dao/ProjectDao.java
@@ -0,0 +1,40 @@
+package ru.javaops.masterjava.persist.dao;
+
+import com.bertoncelj.jdbi.entitymapper.EntityMapperFactory;
+import one.util.streamex.StreamEx;
+import org.skife.jdbi.v2.sqlobject.BindBean;
+import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
+import ru.javaops.masterjava.persist.model.BaseEntity;
+import ru.javaops.masterjava.persist.model.Project;
+
+import java.util.List;
+import java.util.Map;
+
+@RegisterMapperFactory(EntityMapperFactory.class)
+public abstract class ProjectDao implements AbstractDao<Project> {
+
+    @SqlUpdate("TRUNCATE project CASCADE ")
+    @Override
+    public abstract void clean();
+
+    @SqlQuery("SELECT * FROM project ORDER BY name")
+    public abstract List<Project> getAll();
+
+    public Map<String, Project> getAsMap() {
+        return StreamEx.of(getAll()).toMap(Project::getName, g -> g);
+    }
+
+    @SqlUpdate("INSERT INTO project (name, description)  VALUES (:name, :description)")
+    @GetGeneratedKeys
+    public abstract int insertGeneratedId(@BindBean Project project);
+
+    @Override
+    public <T extends Project> T insert(T project) {
+        int id = insertGeneratedId(project);
+        project.setId(id);
+        return project;
+    }
+}

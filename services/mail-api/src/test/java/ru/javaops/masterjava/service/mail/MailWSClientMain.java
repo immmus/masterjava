012f4f85fc7cbde68f5aed7cda0d4df47a082ebb
booktest <fonto.trg@gmail.com>
2019-03-06 16:53:40 +0300

    HW07 - Share wsdl for all modules in \apps_settings\masterjava\config\wsdl. Updated mailService.wsdl in accordance with the implementation (+ fixed problems) and test the service

--- services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
+++ services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
@@ -5,7 +5,7 @@ import com.google.common.collect.ImmutableSet;
 public class MailWSClientMain {
     public static void main(String[] args) {
         MailWSClient.sendToGroup(
-                ImmutableSet.of(new Addressee("To <masterjava@javaops.ru>")),
-                ImmutableSet.of(new Addressee("Copy <masterjava@javaops.ru>")), "Subject", "Body");
+                ImmutableSet.of(new Addressee("To <evdokim.efimov@akbars.ru>")),
+                ImmutableSet.of(new Addressee("Copy <ilya.ksenofontov.internal@akbars.ru>")), "Subject", "Test");
     }
 }

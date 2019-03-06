5affc9d47745920646d8d2b0fd3109b411c9aeec
booktest <fonto.trg@gmail.com>
2019-03-07 00:36:26 +0300

    HW07 - Make a simple interface in the web module for selecting users from a table and sending mail to them.

--- web/webapp/src/main/java/ru/javaops/masterjava/webapp/UsersServlet.java
+++ web/webapp/src/main/java/ru/javaops/masterjava/webapp/UsersServlet.java
@@ -1,16 +1,21 @@
 package ru.javaops.masterjava.webapp;
 
+import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableMap;
+import one.util.streamex.StreamEx;
 import org.thymeleaf.context.WebContext;
+import org.thymeleaf.util.StringUtils;
 import ru.javaops.masterjava.persist.DBIProvider;
 import ru.javaops.masterjava.persist.dao.UserDao;
+import ru.javaops.masterjava.service.mail.Addressee;
+import ru.javaops.masterjava.service.mail.MailWSClient;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Set;
 
 import static ru.javaops.masterjava.common.web.ThymeleafListener.engine;
 
@@ -19,9 +24,20 @@ public class UsersServlet extends HttpServlet {
     private UserDao userDao = DBIProvider.getDao(UserDao.class);
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)  throws IOException {
         final WebContext webContext = new WebContext(req, resp, req.getServletContext(), req.getLocale(),
                 ImmutableMap.of("users", userDao.getWithLimit(20)));
         engine.process("users", webContext, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String emails = req.getParameter("emails");
+        String body = req.getParameter("body");
+        String subject = req.getParameter("subject");
+        if (!StringUtils.isEmpty(emails)) {
+            Set<Addressee> to = StreamEx.of(Splitter.on(',').splitToList(emails)).map(Addressee::new).toSet();
+            MailWSClient.sendBulk(to, subject, body);
+        }
     }
 }
